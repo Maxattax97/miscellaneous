291249bdf2a808388cdba9ff0a780c35124aff28
Max O'Cull <Maxattax97@gmail.com>
2017-07-01 17:44:50 -0400

    Improve CtlPanel

--- control-panel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tmux new-session -d -s "respawn-example"
-tmux set -g remain-on-exit on
-tmux selectp -t 0                      
-tmux split-window -v 'htop'
-tmux -2 attach-session -d 
