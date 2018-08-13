41c86a123d4887fc7bc72ca375cb808127af0f42
Max O'Cull <max.ocull@protonmail.com>
2018-08-12 22:47:15 -0400

    Reorganize directories; add installation script

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+MISC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+echo "Linking from ${MISC_DIR} ..."
+
+link_source() {
+    echo "Linking ${MISC_DIR}/${1} -> ${HOME}/${2:-$1} ..."
+    ln -sf "${MISC_DIR}/${1}" "${HOME}/${2:-$1}"
+}
+
+# Dot files
+link_source .ctags
+link_source .bashrc
+link_source .zshrc
+link_source .tmux.conf
+link_source .tmuxline.conf
+link_source .Xdefaults
+
+# Dot directories
+mkdir -p "${HOME}/.config/"
+
+echo "Backing up ${HOME}/.config ..."
+tar -czf "${HOME}/.config.bak.tar.gz" "${HOME}/.config/"
+
+link_source "config/i3/" ".config/"
+link_source "config/mpd/" ".config/"
+
+# Binaries / executables
+mkdir -p "${HOME}/bin/"
+
+link_source "bin/ctlpanel"
+link_source "bin/gspeak"
+link_source "bin/idle-mine"
+link_source "bin/reload-kde"
+link_source "bin/restart-kde"
+link_source "bin/logout-kde"
+
+echo "Environment installation complete"
