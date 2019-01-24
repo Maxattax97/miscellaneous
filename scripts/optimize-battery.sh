3cacca175cb9f319723fed5731190b2d8b7a153d
Max O'Cull <max.ocull@protonmail.com>
2019-01-24 15:38:59 -0500

    Add additional battery optimizations

--- scripts/optimize-battery.sh
+++ scripts/optimize-battery.sh
@@ -10,5 +10,8 @@ echo 'min_power' > '/sys/class/scsi_host/host1/link_power_management_policy';
 echo 'min_power' > '/sys/class/scsi_host/host2/link_power_management_policy';
 echo 'min_power' > '/sys/class/scsi_host/host3/link_power_management_policy';
 
+# Optimize power usage on AMD graphics.
+echo 'auto' > '/sys/bus/pci/devices/0000:00:01.0/power/control';
+
 # Disable Bluetooth.
 bluetooth off
